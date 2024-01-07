cdccac472323a5177a50c700af4de263b35f2d10
Petar-Dinev <p.dinev55@gmail.com>
2024-01-07 13:34:29 +0200

    Add login page and some css for header element

--- src/App.js
+++ src/App.js
@@ -2,14 +2,18 @@ import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Catalog from './components/Catalog/Catalog';
+import Header from './components/Header/Header';
+import Login from './components/Catalog/Login/Login';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Welcome to our site!</h1>
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='/catalog' element={<Catalog />} />
+        <Route path='/login' element={<Login/>} />
       </Routes>
     </div>
   );
